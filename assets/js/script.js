b47df8b6dd40d179b0077c6fc1c08bbea6beb910
sebastian araya <asistente.sa@gmail.com>
2024-02-17 12:06:03 -0300

    Segunda carga de js en main

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,70 +3,135 @@
 
 //Definición de las estructuras de datos
 let radiologia = [{
-    hora: "10:00",
-    especialista: "<NAME>",
-    paciente: "<NAME>",
-    rut: "12345678",
+    hora: "11:00",
+    especialista: "<ignacio zuñiga>",
+    paciente: "<francisca rojas>",
+    rut: "98787821", //no se que pasa que no acepta el guion del rut
+    prevision: "fonasa"
+},
+{
+    hora: "11:30",
+    especialista: "<federico subercaseaux>",
+    paciente: "<pamela estrada>",
+    rut: "9153452413",
     prevision: "Isapre"
 },
 {
+    hora: "15:00",
+    especialista: "<fernando wurthz>",
+    paciente: "<armando luna>",
+    rut: "1164453459",
+    prevision: "isapre"
+},
+{
+    hora: "15:30",
+    especialista: "<ana maria godoy>",
+    paciente: "<manuel godoy>",
+    rut: "176664190",
+    prevision: "Fonasa"
+},
+{
+    hora: "16:00",
+    especialista: "<patricia suazo>",
+    paciente: "<ramon ulloa>",
+    rut: "114989389k",
+    prevision: "Fonasa"
+}
+
+];
+let traumatologia = [{
+    hora: "8:00",
+    especialista: "<maria pas altuzarra>",
+    paciente: "<paula sanchez>",
+    rut: "155547745",
+    prevision: "fonasa"
+},
+{
+    hora: "10:00",
+    especialista: "<raul araya>",
+    paciente: "<angelica navas>",
+    rut: "1154441479",
+    prevision: "Isapre"
+},
+{
+    hora: "10:30",
+    especialista: "<maria arriagada>",
+    paciente: "<ana klapp>",
+    rut: "178794239",
+    prevision: "isapre"
+},
+{
+    hora: "11:00",
+    especialista: "<alejandro badilla>",
+    paciente: "<felipe mardones>",
+    rut: "15474236",
+    prevision: "isapre"
+},
+{
+    hora: "11:30",
+    especialista: "<cecilia budnik>",
+    paciente: "<diego marre>",
+    rut: "16554741k",
+    prevision: "fonasa"
+},
+{
     hora: "12:00",
-    especialista: "<NAME1>",
-    paciente: "<NAME>",
-    rut: "12345678",
+    especialista: "<arturo cavagnaro>",
+    paciente: "<cecilia mendez>",
+    rut: "197475358",
+    prevision: "isapre"
+},
+{
+    hora: "12:30",
+    especialista: "<andres kanacri>",
+    paciente: "<marcial suazo>",
+    rut: "112547855",
+    prevision: "Fisapre"
+}
+
+];
+let dental = [{
+    hora: "08:30",
+    especialista: "<andrea zuñiga>",
+    paciente: "<marcela retamal>",
+    rut: "11123456", //no se que pasa que no acepta el guion del rut
+    prevision: "Isapre"
+},
+{
+    hora: "11:00",
+    especialista: "<maria pia zañartu>",
+    paciente: "<angel muñoz>",
+    rut: "98787892",
+    prevision: "Isapre"
+},
+{
+    hora: "11:30",
+    especialista: "<scarlett witting>",
+    paciente: "<mario kast>",
+    rut: "179987895",
     prevision: "Fonasa"
 },
 {
     hora: "13:00",
-    especialista: "<NAME2>",
-    paciente: "<NAME>",
-    rut: "12345678",
-    prevision: "Fonasa"
-}
-];
-let traumatologia = [{
-    hora: "10:00",
-    especialista: "<NAME3>",
-    paciente: "<NAME3>",
-    rut: "12345678",
-    prevision: "Isapre"
-},
-{
-    hora: "10:00",
-    especialista: "<NAME4>",
-    paciente: "<NAME4>",
-    rut: "12345678",
-    prevision: "Isapre"
-},
-{
-    hora: "10:00",
-    especialista: "<NAME5>",
-    paciente: "<NAME5>",
-    rut: "12345678",
-    prevision: "Fonasa"
-}
-];
-let dental = [{
-    hora: "10:00",
-    especialista: "<NAME6>",
-    paciente: "<NAME6>",
-    rut: "12345678",
+    especialista: "<francisco von tueber>",
+    paciente: "<karin fernandez>",
+    rut: "18887662-k",
     prevision: "Fonasa"
 },
 {
-    hora: "10:00",
-    especialista: "<NAME7>",
-    paciente: "<NAME7>",
-    rut: "12345678",
+    hora: "13:30",
+    especialista: "<eduardo viñuela>",
+    paciente: "<hugo sanchez>",
+    rut: "17665414",
     prevision: "Fonasa"
 },
 {
-    hora: "10:00",
-    especialista: "<NAME8>",
-    paciente: "<NAME8>",
-    rut: "12345678",
-    prevision: "Isapre"
-}
+    hora: "14:00",
+    especialista: "<raquel villaseca>",
+    paciente: "<ana sepulveda>",
+    rut: "144412810",
+    prevision: "isapre"
+},
 ];
 
 
